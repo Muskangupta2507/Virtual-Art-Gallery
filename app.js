184674e21dd6120473117c701ed80724366594c0
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-06-15 14:24:44 +0530

    initial setup

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import path from 'path';
 import fs from 'fs';
 import cookieParser from 'cookie-parser';
+import ejsMate from 'ejs-mate';
+import methodOverride from 'method-override';
 // import authRoutes from './routes/auth.routes.js';
 // import playlistRoutes from './routes/playlist.routes.js';
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+app.engine('ejs',ejsMate);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, 'views'));
 app.use(express.json()) //to parse the incoming requests with JSON payloads(from body of request)
 app.use(cookieParser()); // to parse through cookies
 
@@ -20,6 +25,9 @@ if (!fs.existsSync(uploadDir)) {
 
 // Middleware to serve static files from the backend/public/uploads directory
 app.use('/uploads', express.static(uploadDir));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get("/",(req,res)=>{
     res.send("hello");
